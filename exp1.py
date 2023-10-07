31e65a1f7b1d65eac28f6d0546b4cf3fc22a078d
swethaps2000 <120661245+swethaps2000@users.noreply.github.com>
2023-10-07 15:25:27 +0530

    Add files via upload

--- exp1.py
+++ exp1.py
@@ -215,6 +215,274 @@ else:
     print(c," is the Largest number")
 
 
+# In[2]:
+
+
+limit=int(input("Enter a limit:"))
+Sum=0
+for i in range(1,limit+1):
+    if i%2!=0:
+        Sum=Sum+i
+print(Sum)
+
+
+# In[1]:
+
+
+l=int(input("Enter a limit: "))
+for i in range(1,l+1):
+    print(i)
+
+
+# In[11]:
+
+
+a=int(input("Enter a number:"))
+Sum=0
+while(a>0):
+    rem=a%10
+    Sum=Sum+rem
+    a=a//10
+print(Sum)
+
+
+# In[39]:
+
+
+n=int(input("Enter a limit:"))
+for i in range(1,n+1):
+    odd=False
+    if(i%2!=0):
+        odd=True
+    if odd:
+        print(i)
+        
+        
+        
+
+
+# In[41]:
+
+
+l1=[1,3.5,'a','b',True]
+print(l1)
+l1.append("c")
+print(l1)
+
+
+# In[42]:
+
+
+print(l1.pop())
+
+
+# In[49]:
+
+
+l1.insert(3,"o")
+print(l1)
+
+
+# In[50]:
+
+
+l1.remove("a")
+print(l1)
+
+
+# In[51]:
+
+
+l1=[2,3,'a']
+l2=[1,'b']
+l1.append(l2)
+print(l1)
+
+
+# In[52]:
+
+
+l1.remove(l2)
+print(l1)
+
+
+# In[54]:
+
+
+l1.extend(l2)
+print(l1)
+
+
+# In[56]:
+
+
+a=[5,8,1,10,67,3,4]
+a.sort()
+print(a)
+
+
+# In[67]:
+
+
+num=[1,2,3,4,5]
+sq=[]
+for i in num:
+    sq.append(pow(i,2))
+print(sq)
+    
+
+
+# In[68]:
+
+
+numbers=[0,1,2,3,4,5,6,7,8,9,10]
+print(numbers[1],numbers[-1])
+
+
+# In[69]:
+
+
+sliced=numbers[5:11]
+print(sliced)
+
+
+# In[71]:
+
+
+slice1=numbers[5:]
+print(slice1)
+
+
+# In[72]:
+
+
+sliced=numbers[:7]
+print(sliced)
+
+
+# In[73]:
+
+
+slice2=numbers[-2:]
+print(slice2)
+
+
+# In[77]:
+
+
+numbers=[*range(1,8)]
+print(numbers)
+
+
+# In[78]:
+
+
+square=[x**2 for x in numbers]
+print(square)
+
+
+# In[80]:
+
+
+odd_square=[x**2 for x in numbers if x%2!=0]
+print(odd_square)
+
+
+# In[81]:
+
+
+A=[4,6,8,9]
+AxA=[(a,b) for a in A for b in A if a!=b ]
+print(AxA)
+
+
+# In[85]:
+
+
+student={'name':'swetha','age':22}
+print(student['name'])
+
+
+# In[86]:
+
+
+print('name' in student)
+
+
+# In[88]:
+
+
+print('class' in student)
+
+
+# In[89]:
+
+
+student['class']="MCA"
+print(student)
+
+
+# In[90]:
+
+
+for item in student:
+    print(item,student[item])
+
+
+# In[96]:
+
+
+for(key,value) in student.items():
+    print(key.capitalize(),'\t:\t',value)
+print(student.keys())
+
+
+# In[97]:
+
+
+def square(number):
+    return pow(number,2)
+s=square(5)
+print(s)
+
+
+# In[98]:
+
+
+def isPrime(number):
+    for factor in range(2, (number//2)+1):
+        if number%factor == 0:
+            return False
+        return True
+
+number = int(input('Enter the number '))
+print(isPrime(number))
+
+
+# In[100]:
+
+
+def printPrimes(llimit, ulimit):
+    for num in range(llimit,ulimit+1):
+        if isPrime(num)==True:
+            print(num,end=' ')
+printPrimes(5,20)
+
+
+# In[101]:
+
+
+def swap(x,y):
+    t=x
+    x=y
+    y=t
+    return x,y
+a=5
+b=7
+a,b=swap(a,b)
+print(a,b)
+
+
 # In[ ]:
 
 
